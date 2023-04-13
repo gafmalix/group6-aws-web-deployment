c417be0e94a2d8b0d4b0db723eb50daa00bef938
gafmalix <79617087+gafmalix@users.noreply.github.com>
2023-04-12 18:59:53 -0600

    initial commit

---
+++ src/main/java/com/group6/demo/controller/PersonController.java
@@ -0,0 +1,13 @@
+package com.group6.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+	@RequestMapping("/greetGroupName")
+	public String greetGroupName() {
+		return "Hello Group 6";
+	}
+}
